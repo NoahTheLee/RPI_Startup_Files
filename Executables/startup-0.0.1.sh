656ef3bc1166660cf9d9d30f96d1fbaee9dc624c
Noah Lee <noahjefferson.lee@calbaptist.edu>
2024-10-30 14:11:48 -0700

    Small updates to startup

---
+++ Executables/startup-0.0.1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Pairing of basic bluetooth peripherals
+pair_bluetooth() {
+    echo "Pairing bluetooth devices..."
+
+}
+
+# Removing apps that are not needed
+uninstall_unwanted_apps() {
+    echo "Uninstalling unwanted apps..."
+
+}
+
+# Installing apps that I'd like to include
+install_necessary_apps() {
+    echo "Installing necessary apps..."
+
+}
+
+# Setting a background image the desktop
+set_background(){
+    echo "Setting background image..."
+
+}
+
+pair_bluetooth
+set_background
+
+echo "Setup completed!"

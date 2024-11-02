e9582c2abc2fe95137d2564e424a03d6fb333c7c
Noah Lee <noahjefferson.lee@calbaptist.edu>
2024-11-02 11:13:46 -0700

    Silenced warning on apt list

--- Other Programs/Coursework/CSC321/installers/csc321_webservers.sh
+++ Other Programs/Coursework/CSC321/installers/csc321_webservers.sh
@@ -3,4 +3,4 @@ sudo apt install -y apache2 ;
 echo "Installing Nginx" ;
 sudo apt install -y nginx ;
 echo "Installed webservers:" ;
-apt list --installed | grep -e apache2 -e nginx
+apt list --installed 2>/dev/null | grep -e apache2 -e nginx

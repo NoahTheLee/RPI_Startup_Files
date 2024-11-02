6e56c122404d5a3c895ed20c17fdaad5912d5764
Noah Lee <noahjefferson.lee@calbaptist.edu>
2024-11-02 11:11:39 -0700

    Removed whitespace

--- Other Programs/Coursework/CSC321/installers/csc321_webservers.sh
+++ Other Programs/Coursework/CSC321/installers/csc321_webservers.sh
@@ -1,8 +1,6 @@
 echo "Installing Apache2" ;
 sudo apt install -y apache2 ;
-
 echo "Installing Nginx" ;
 sudo apt install -y nginx ;
-
 echo "Installed webservers:" ;
 apt list --installed | grep -e apache2 -e nginx
